57b8e7a0cb3e1ff7a630a09ab3414e7bad78e5d1
pawel idzikowski <iddziku@gmail.com>
2022-07-29 17:38:43 +0200

    Create markedCommentsTest.js

---
+++ markedCommentsTest.js
@@ -0,0 +1,17 @@
+// @TODO this is valid single-line comment
+
+const x = 2; /* this is invalid comment */
+
+console.log("test123"); // @NOTE hello
+
+/*
+ * this is
+ * invalid
+ * comment
+ */
+
+/*
+ * this is
+ * @TMP valid
+ * comment
+ */
