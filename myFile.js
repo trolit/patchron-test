267a48d5289bbacee22d1f3dcb559e1ebdccff40
pawel idzikowski <iddziku@gmail.com>
2022-07-31 13:43:40 +0200

    Create myFile.js

---
+++ myFile.js
@@ -0,0 +1,13 @@
+const html2canvas = require('html2canvas');
+const test1 = require('@/helpers/helper1');
+const test2 = require('@/helpers/helper2');
+
+module.exports = (a, b) => {
+    const sum = a + b;
+
+    return sum;
+};
+
+const lodash = require('lodash');
+
+const test3 = require('@/helpers/helper3');
