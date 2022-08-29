300253b94a3bd616de398c90582ba97dd316dc1a
pawel idzikowski <iddziku@gmail.com>
2022-08-29 22:44:27 +0200

    tt

---
+++ client/src/helpers/doSomething.js
@@ -0,0 +1,27 @@
+// invalid
+import pinoMultiTree from 'pino-multi-tree';
+import Test123 from '@/components/Test123.vue';
+import lodash from 'lodash';
+import {
+    test1,
+    test2,
+    test3,
+    test4,
+    test5,
+    test6
+} from '@/components/Test1.vue';
+import Test1234567 from '@/components/Test1234567.vue';
+
+// valid
+import lodash from 'lodash';
+import pinoMultiTree from 'pino-multi-tree';
+import {
+    test1,
+    test2,
+    test3,
+    test4,
+    test5,
+    test6
+} from '@/components/Test1.vue';
+import Test123 from '@/components/Test123.vue';
+import Test1234567 from '@/components/Test1234567.vue';
